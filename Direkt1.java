1cb95d4ad9eafa1f31e072df3ff30410d9322b97
SpaderP <Pereirapatrick84@Gmail.com>
2018-08-13 18:22:30 +0200

    Create Direkt1.java

---
+++ Direkt1.java
@@ -0,0 +1,14 @@
+//filnamn Direkt1.java
+import java.awt.*;
+import javax.swing.*;
+
+public class Direkt1 extends JApplet {
+public void init() {
+
+  JLabel l = new JLabel("Patrick Pereira", JLabel.CENTER);add(l);
+  l.setBackground(new Color(255,200,0));
+  l.setForeground(new Color(124,96,78));
+  l.setFont(new Font("SansSerif", Font.BOLD, 14));
+
+   }
+}
